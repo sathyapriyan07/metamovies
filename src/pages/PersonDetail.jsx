0c8d4e9d280064eab0f5f838bf91982f3474468a
Sathya Priyan <sathyapriyan710@gmail.com>
2026-02-08 14:52:46 +0530

    Increase filmography grid on person detail page

--- src/pages/PersonDetail.jsx
+++ src/pages/PersonDetail.jsx
@@ -155,7 +155,7 @@ const PersonDetail = () => {
             )}
 
             {activeTab === 'filmography' && (
-              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              <div className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-4">
                 {allWorks.map((work, i) => (
                   <div
                     key={i}
